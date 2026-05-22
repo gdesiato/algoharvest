2c13e9d51b1897c6a752cbfbcdab3a6eee9ecf60
Giuseppe Desiato <giodesiato@gmail.com>
2026-05-22 18:22:25 +0200

    fixed supabase file

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   'https://yuqdvbsarpnoxqwryjjz.supabase.co',
-  'sb_publishable_hwAI8TrkDt-FXyilswjZXA_ZP56S1lb'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1cWR2YnNhcnBub3hxd3J5amp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NTU2NzAsImV4cCI6MjA5NTAzMTY3MH0.QXGmxaGSoLaeYy7ihXHE_JE65Nnuc7x6MqjmbOn9VVY'
 );
